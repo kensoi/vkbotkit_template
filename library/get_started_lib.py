99636cbb2bb21da277a9f6e8d61828dab8135231
kensoi <65781959+kensoi@users.noreply.github.com>
2022-10-26 22:10:27 +0500

    v1.2a4 refactoring

--- library/get_started_lib.py
+++ library/get_started_lib.py
@@ -2,9 +2,9 @@
 Copyright 2022 kensoi
 """
 
-from vkbotkit.objects import callback, filters, Library
-
-
+from vkbotkit.objects import callback, Library
+from vkbotkit.objects.filters.actions import ChatInviteUser
+from vkbotkit.objects.filters.message import IsThatText, IsCommand
 HELLO_MESSAGE = """
 Hello, world
 Programmed to work and not to feel
@@ -15,38 +15,24 @@ HELP INSTRUCTIONS HERE
 """
 
 
-class NewUser(filters.Filter):
-    """
-    Фильтр оповещений о новых участниках
-    """
-
-    async def check(self, package):
-        if not package.action:
-            return
-
-        if not hasattr(package.action, "type"):
-            return
-
-        return package.action == "chat_invite_user"
-
-
 class Main(Library):
     """
     Библиотека с командами, предназначенными для приветствия пользователей
     """
 
-    @callback(filters.IsCommand({"start",}) | NewUser())
-    async def send_hello(self, package, toolkit):
+    @callback(IsCommand({"start",}) | IsThatText({"Начать",}) | ChatInviteUser())
+    async def send_hello(self, toolkit, package):
         """
         при получении команды '@your_bot_id start' => отправлять текст HELLO_ME
         """
-        await toolkit.send_reply(package, HELLO_MESSAGE)
+        await toolkit.messages.reply(package, HELLO_MESSAGE)
 
 
-    @callback(filters.IsCommand({"help",}))
-    async def send_help(self, package, toolkit):
+    @callback(IsCommand({"help",}) | IsThatText({"Помощь",}))
+    async def send_help(self, toolkit, package):
         """
         при получении команды '@your_bot_id start' => отправлять текст HELLO_ME
         """
-        await toolkit.send_reply(package, HELP_MESSAGE)
+
+        await toolkit.messages.reply(package, HELP_MESSAGE)
         
